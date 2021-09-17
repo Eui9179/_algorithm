631a72a5b40f791f7ebfcc8151e356574a5b8891
eui9179 <outdark97@gmail.com>
2021-09-17 11:27:15 +0900

    quickSort

---
+++ src/sort/QuickSort.java
@@ -0,0 +1,44 @@
+package sort;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class QuickSort {
+
+    public ArrayList<Integer> sort(ArrayList<Integer> dataList){
+        if (dataList.size() <= 1) {
+            return dataList;
+        }
+        int pivot = dataList.get(0);
+
+        ArrayList<Integer> leftArr = new ArrayList<Integer>();
+        ArrayList<Integer> rightArr = new ArrayList<Integer>();
+
+        for (int index = 1; index < dataList.size(); index++) {
+            if (dataList.get(index) > pivot) {
+                rightArr.add(dataList.get(index));
+            } else {
+                leftArr.add(dataList.get(index));
+            }
+        }
+
+        ArrayList<Integer> mergedArr = new ArrayList<Integer>();
+        mergedArr.addAll(this.sort(leftArr));
+        mergedArr.add(pivot);
+        mergedArr.addAll(this.sort(rightArr));
+
+        return mergedArr;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> testData = new ArrayList<Integer>();
+
+        for (int index = 0; index < 100; index++) {
+            testData.add((int) (Math.random() * 100));
+        }
+
+        QuickSort qs = new QuickSort();
+        System.out.println(qs.sort(testData));
+    }
+
+}
